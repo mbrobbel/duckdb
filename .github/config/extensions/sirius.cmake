d14387c63c7bcd41ecb40c7493b4fc4924cc17a3
Matthijs Brobbel <m1brobbel@gmail.com>
2026-09-23 17:18:06 +0000

    Probe Sirius fork with compatible vcpkg configuration

--- .github/config/extensions/sirius.cmake
+++ .github/config/extensions/sirius.cmake
@@ -1,7 +1,7 @@
 duckdb_extension_load(sirius
     DONT_LINK LOAD_TESTS
-    GIT_URL https://github.com/sirius-db/sirius
-    GIT_TAG 9912b4456117c1f578f5f91e7514387eeeaf0100
+    GIT_URL https://github.com/mbrobbel/sirius
+    GIT_TAG b8de6bac699938e6f2bfdfdc334fb4729ee33642
     INCLUDE_DIR src
     TEST_DIR test/sql
     SUBMODULES "duckdb;cucascade;substrait"
